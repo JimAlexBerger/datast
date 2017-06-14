9c76f56d67481a89160cc0c065beb69bd209fa44
JimAlexBerger <alexandergundersen@yaho.no>
2017-06-14 22:14:32 +0200

    Datast V0.1.0
    Første fungerende versjon

--- public/js/datast.js
+++ public/js/datast.js
@@ -1,3 +1,19 @@
-document.onload = function(){
-    firebase.auth().signInWithPopup(provider);
+window.onload = function() {
+    document.getElementById("renderBtn").onclick = render;
+}
+
+function render() {
+    var css = document.getElementById("CssPane").innerHTML;
+    var html = document.getElementById("HTMLPane").innerHTML;
+    var js = document.getElementById("javascriptPane").innerHTML;
+    var result = '<html><head><style>' + css + '</style></head><body>' + html + '<script type="text/javascript">' + js + '</script></body></html>'
+    var iframe = document.getElementById('OutputPane');
+
+    if (iframe.contentDocument) doc = iframe.contentDocument;
+    else if (iframe.contentWindow) doc = iframe.contentWindow.document;
+    else doc = iframe.document;
+
+    doc.open();
+    doc.writeln(result);
+    doc.close();
 }
