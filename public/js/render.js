d8ecff1df50c31de1521a9456a2d8eb5cabdb0cd
JimAlexBerger <alexandergundersen@yaho.no>
2017-06-14 22:44:49 +0200

    separerte render funksjonalitet vekk fra main kode

---
+++ public/js/render.js
@@ -0,0 +1,15 @@
+function renderValues(){
+    var css = document.getElementById("CssPane").innerHTML;
+    var html = document.getElementById("HTMLPane").innerHTML;
+    var js = document.getElementById("javascriptPane").innerHTML;
+    var result = '<html><head><style>' + css + '</style></head><body>' + html + '<script type="text/javascript">' + js + '</script></body></html>'
+    var iframe = document.getElementById('OutputPane');
+
+    if (iframe.contentDocument) doc = iframe.contentDocument;
+    else if (iframe.contentWindow) doc = iframe.contentWindow.document;
+    else doc = iframe.document;
+
+    doc.open();
+    doc.writeln(result);
+    doc.close();
+}
