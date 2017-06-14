c934c5072cd61644207cf47205c0f2f26a0229c1
Tiddi <tidemann.tiddi@hotmail.com>
2017-06-14 20:48:12 +0200

    la til firebase init script

---
+++ public/js/firebaseInit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCWo8BV5Su4ASaNj-JE2nr5p4QGHKtSPWA",
+    authDomain: "datast-24d32.firebaseapp.com",
+    databaseURL: "https://datast-24d32.firebaseio.com",
+    projectId: "datast-24d32",
+    storageBucket: "datast-24d32.appspot.com",
+    messagingSenderId: "671327180228"
+};
+firebase.initializeApp(config);
